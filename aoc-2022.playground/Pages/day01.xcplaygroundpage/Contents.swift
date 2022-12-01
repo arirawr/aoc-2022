39fa199fa49f4b7175235ec40e4a7821848c9aa3
Ari Vaniderstine <ari.vaniderstine@king.com>
2022-12-01 16:57:06 +0100

    Day 1 refactor, add raw input helper

--- aoc-2022.playground/Pages/day01.xcplaygroundpage/Contents.swift
+++ aoc-2022.playground/Pages/day01.xcplaygroundpage/Contents.swift
@@ -2,48 +2,29 @@
 
 import Foundation
 
-let input = readInput().map({ Int($0) })
+let input = readInputRaw()
 
 func part1() {
-    var sum = 0
-    var highestSum = 0
-
-    for i in input {
-        if i != nil {
-            sum += i!
-        }
-        // If nil, we have an empty line and should reset the sum
-        else {
-            if sum > highestSum {
-                highestSum = sum
-            }
-            sum = 0
-        }
-    }
-
-    print(highestSum)
+    print(input
+        .components(separatedBy: "\n\n")
+        .map({ $0
+            .split(separator: "\n")
+            .map( {Int($0)!} )
+        .reduce(0,+) })
+        .sorted()
+        .suffix(1))
 }
 
 func part2() {
-    var sum = 0
-    var highestSums = [Int]()
-
-    for i in input {
-        if i != nil {
-            sum += i!
-        }
-        // If nil, we have an empty line and should reset the sum
-        else {
-            highestSums.append(sum)
-            sum = 0
-        }
-    }
-    
-    // Sort by value (ascending)
-    highestSums.sort()
-
-    // Sum of the 3 highest values
-    print(highestSums.suffix(3).reduce(0,+))
+    print(input
+        .components(separatedBy: "\n\n")
+        .map({ $0
+            .split(separator: "\n")
+            .map( {Int($0)!} )
+            .reduce(0,+) })
+        .sorted()
+        .suffix(3)
+        .reduce(0,+))
 }
 
 part1()
--- aoc-2022.playground/Sources/util.swift
+++ aoc-2022.playground/Sources/util.swift
@@ -1,9 +1,19 @@
 import Foundation
 
-public func readInput() -> [String] {
+public func readInputArray() -> [String] {
     guard let url = Bundle.main.url(forResource: "input", withExtension: ".txt") else { fatalError() }
     do {
         return try String(contentsOf: url).components(separatedBy: "\n")
+    } catch {
+        print(error.localizedDescription)
+        fatalError()
+    }
+}
+
+public func readInputRaw() -> String {
+    guard let url = Bundle.main.url(forResource: "input", withExtension: ".txt") else { fatalError() }
+    do {
+        return try String(contentsOf: url)
     } catch {
         print(error.localizedDescription)
         fatalError()
